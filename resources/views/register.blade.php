839b6b1e6d9cbfb7bca71dc09e103781ef1a39fb
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-11 12:23:21 +0600

    1_making authcontroller & register form_p1

---
+++ resources/views/register.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 Practicing with custom Auth</title>
+</head>
+<body>
+
+    <h2>Register Form</h2>
+    <form action="">
+        <label for="name">Name</label><br>
+        <input type="text" name="name" id="name" placeholder="Sohan Sujan" required><br>
+        <label for="email">Email</label><br>
+        <input type="email" name="email" id="email" placeholder="email@example.com" required><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required><br><br>
+        <button>Register</button>
+
+    </form>
+
+</body>
+</html>
