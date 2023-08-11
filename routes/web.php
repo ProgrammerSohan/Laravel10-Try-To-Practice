401d2b2ad97e9ff887a3492d75e0aa293744bd8c
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-11 16:19:49 +0600

    4_login to home page_p4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,4 @@ Route::get('/register',[AuthController::class,'register'])->name('register');
 Route::post('/register',[AuthController::class,'registerPost'])->name('register');
 Route::get('/login', [AuthController::class,'login'])->name('login');
 Route::post('/login', [AuthController::class,'loginPost'])->name('login');
+Route::get('/home',[HomeController::class,'index']);
